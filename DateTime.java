3ce17e4f6b87af798b171f6000bac5194ddabde0
tejassbhosale0812 <tejassbhosale0812@gmail.com>
2026-05-09 13:21:42 +0530

    Create DateTime.java

---
+++ DateTime.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class DateTime {
+    public static void main(String[] args) {
+
+        Date d = new Date();
+
+        System.out.println("Current Date and Time:");
+        System.out.println(d);
+    }
+}
